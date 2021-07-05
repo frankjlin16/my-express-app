db09cc630e039c3326fa2c995a500846982edaa0
frankjlin16 <flin30@asu.edu>
2021-07-05 14:02:00 -0700

    Modify views

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var TopicModel = require('../models/topics')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
 
-  res.render('index', { currentURL: '/' });
+  res.render('index', {
+    currentURL: '/'
+  });
 });
 
+// Show all topics
 router.get('/topics', function (req, res, next) {
 
+  var topics = TopicModel.find();
   res.render('topics', {
     currentURL: '/topics'
   })
-})
+});
 
-router.get('/new-entry', function (req, res, next) {
-  res.render('new-entry', {
-    currentURL: "/new-entry"
+
+router.get('/new-topic', function (req, res, next) {
+  res.render('new-topic', {
+    currentURL: "/new-topic"
   })
 })
 
-router.post('/', function (req, res, next) {
+router.post('/new-topic', function (req, res, next) {
   res.send('Got a POST request');
 });
 
