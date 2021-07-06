f9616502a0b6dddae2c03d55d9323a7f94c74005
frankjlin16 <flin30@asu.edu>
2021-07-05 20:03:46 -0700

    Modify topics model

--- models/topics.js
+++ models/topics.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var TopicsSchema = new mongoose.Schema({
     topic: {type: String, required: [true, 'Need a topic title.'] },
     dateCreated: {type: Date, default: Date.now()},
+    entries: [{type: mongoose.Schema.Types.ObjectId, ref: 'Entry'}]
 });
 
 var TopicModel = mongoose.model('TopicModel', TopicsSchema);
